22e7c101c043d9ee55805ec6d25d047374164bd1
Dubemxe <dubemxe@gmail.com>
2024-07-01 17:23:35 +0000

    Required files + Task 1

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''
+Module for tests on the utils file
+'''
+from parameterized import parameterized
+import unittest
+from utils import (access_nested_map, get_json, memoize)
+from unittest.mock import patch
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Class to test access_nested_map function"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Checks if the method runs correctly"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Checks if a KeyError is raised when expected"""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
